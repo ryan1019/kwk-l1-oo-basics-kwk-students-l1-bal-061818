85b87b4c852a9d478bbe15088890060ac109a705
ryan1019 <ryan.greathouse05@gmail.com>
2018-06-22 16:18:43 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
-# book.rb
-# Add you Book class here
+class Book
+
+  attr_accessor :author, :page_count, : :genre
+
+
+  attr_reader :title
+  
+
+  def initialize(title)
+    @title = title
+  end
+
+
+
+  def author = (author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count = (pages)
+    @page_count = pages
