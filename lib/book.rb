2ee454df30ca8a726604b3c76f4b90ebd0295ff4
ryan1019 <ryan.greathouse05@gmail.com>
2018-06-22 16:33:58 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,23 +3,41 @@ class Book
   #getter and setter
   attr_accessor :author, :page_count, : :genre
 
-  #to produce just a getter 
+  #to produce just a getter
   attr_reader :title
 
-
+  #to produce just setter
+  #attr_writer :thing
   def initialize(title)
     @title = title
   end
 
+#def title
+#  @title
+# end
+# def author = (author)
+# @author/ = author
+# end
+#
+# def author
+#  @author/
+# end
+# def page_count = (pages)
+#  @page_count = pages
+# end
+# def page_count
+#  @page_count
+# end
+# def genre = (genre)
+#  @genre = genre
+# end
+# def genre
+#  @genre
+# end
+def turn_page
+  puts "Flipping the page... wow, you read fast!"
+end
+end
 
-
-  def author = (author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count = (pages)
-    @page_count = pages
+Harry_Potter = Book.new ("Harry Potter and the Sorcerer's Stone")
+Harry_Potter.turn_page
